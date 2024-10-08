d4fcd51e0e6c93b5605c9e4d33e3c288031bd3a4
Evaeadao2005 <86280981+Evaeadao2005@users.noreply.github.com>
2024-10-08 14:36:12 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,6 +3,7 @@ const carrosselPromocoes = document.getElementById('carrossel-promocoes-fixas');
 const scrollAmount = 210; // Ajuste conforme necessário
 const scrollInterval = 3000; // Tempo entre as rolagens
 
+// Função para rolar automaticamente
 function autoScroll(element) {
     element.scrollBy({
         left: scrollAmount,
@@ -10,13 +11,16 @@ function autoScroll(element) {
     });
 }
 
+// Iniciar a rolagem automática dos carrosséis
 setInterval(() => autoScroll(carrosselOfertas), scrollInterval);
 setInterval(() => autoScroll(carrosselPromocoes), scrollInterval);
 
+// Ação ao clicar no botão de contato
 document.getElementById("contato-button").onclick = function() {
     document.getElementById("popup").classList.toggle("popup-hidden");
 };
 
+// Ação para fechar o popup
 document.querySelector(".close").onclick = function() {
     document.getElementById("popup").classList.add("popup-hidden");
 };
